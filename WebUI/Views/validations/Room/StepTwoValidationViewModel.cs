f2a9a077be36aa6415385dcb1e82f45e68f50218
m.sargazi <m.sargazidev@gamil.com>
2020-07-16 02:37:11 -0700

    add ajax handler

---
+++ WebUI/Views/validations/Room/StepTwoValidationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Views.validations.Room
+{
+    public class StepTwoValidationViewModel
+    {
+        public IList<IFormFile> Files { get; set; }
+    }
+}
